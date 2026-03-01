729046b5cc1334f67ce0ff87d04bd46feb31c3ca
Marie Louise Quitoriano <mariequitoriano24@gmail.com>
2026-03-01 23:43:19 +0800

    Update matmul.c

--- Dy_Quitoriano_SWE_02/matmul.c
+++ Dy_Quitoriano_SWE_02/matmul.c
@@ -4,31 +4,85 @@
 
 #include <sys/time.h>
 
-#define n 3
-
-
 // taken from Introduction to HPC for Scientists and Engineers
 void get_walltime_(double* wcTime) {
     struct timeval tp;
     gettimeofday(&tp, NULL);
     *wcTime = (double)(tp.tv_sec + tp.tv_usec/1000000.0);
 }
-void get_walltime(double* wcTime) {
-    get_walltime_(wcTime);  
+
+void fill_random(float *M, int n) { //  generate random matrix values
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            M[i*n + j] = (float)rand() / RAND_MAX;
 }
 
+void zero_matrix(float *M, int n) { // instantiate an empty matrix for C
+    for (int i = 0; i < n*n; i++)
+        M[i] = 0.0f;
+}
+
+/* make a matrix storage first that is continguous memory row major order:
+[ a00 a01 a02
+  a10 a11 a12
+  a20 a21 a22 ]
+  Stored as a 1D array a_ij
+  [a00 a01 a02 a10 a11 a12 a20 a21 a22]
+  [A[0] A[1] A[2] A[3] A[4] A[5] A[6] A[7] A[8]]
+  if I want to access a12 for n=3 (3x3 matrix), i would need A[(3*1) + 2] = A[5]
+  to access elements in row major order: A[(i*n) + j]
+*/
 
 // C = A * B implementation:
 // its like: i x k mult k x j = i x j
 void mmm(float **A, float **B, float **C, int n){
     for (int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            C[i][j] = 0.0f;
+            //C[i][j] = 0.0f;
+            C[(i*n) + j] = 0.0f;
             for(int k = 0; k < n; k++){
-                C[i][j] += A[i][k] * B[k][j];
+                //C[i][j] += A[i][k] * B[k][j];
+                C[(i*n) + j] += A[(i*n) + k] * B[(k*n) + j];
             }
         }
             
     }
+
+}
+
+int main(){
+    int n;
+    int trials = 10; // can change to 30
+
+    printf("Enter matrix size n: ");
+    scanf("%d", &n);
+
+    // Contiguous memory for A, B, C
+    float *A = malloc(n * n * sizeof(float));
+    float *B = malloc(n * n * sizeof(float));
+    float *C = malloc(n * n * sizeof(float));
+
+    // fill w random values
+    fill_random(A, n);
+    fill_random(B, n);
+
+    double t_start, t_end;
+    double total_time = 0.0;
+
+    for int(t = 0; t < trials; t++){
+        zero_matrix(C, n);
+        get_walltime(&t_start);
+        mmm_ijk(A, B, C, n);
+        get_walltime(&t_end);
+        total_time += (t_end - t_start);
+    }
+
+    printf("Average time over %d trials, takes %f seconds\n",
+    trials, total_time / trials);
+
+    free(A);
+    free(B);
+    free(C);
+    return 0;
 
 }
