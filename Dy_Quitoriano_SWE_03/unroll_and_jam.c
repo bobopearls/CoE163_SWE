a5ca09444e571125499ca8c870eb77a8481b1e3a
Marie Louise Quitoriano <mariequitoriano24@gmail.com>
2026-03-11 22:05:12 +0800

    Create unroll_and_jam.c

---
+++ Dy_Quitoriano_SWE_03/unroll_and_jam.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#include <sys/time.h>
+
+// taken from Introduction to HPC for Scientists and Engineers
+void get_walltime_(double* wcTime) {
+    struct timeval tp;
+    gettimeofday(&tp, NULL);
+    *wcTime = (double)(tp.tv_sec + tp.tv_usec/1000000.0);
+}
+
+/*
+void transpose(float *A, float *B ,int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            //A[j][i] = B[i][j];
+            A[j*n + i] = B[i*n + j];
+        }
+    }
+}
+*/
+void transpose_unroll_jam(float *A, float *B, int n){
+    int i, j;
+    int unroll_depth = 4;
+
+    // UNROLL the outer loop
+    for(i = 0; i <= i - unroll_depth; i+= unroll_depth){
+        for(j = 0; j < n; j++){
+            // JAM inner loop
+            A[j*n +(i + 0)] = B[(i + 0) * n + j];
+            A[j*n +(i + 1)] = B[(i + 1) * n + j];
+            A[j*n +(i + 2)] = B[(i + 2) * n + j];
+            A[j*n +(i + 3)] = B[(i + 3) * n + j];
+        }
+    }
+    // REMAINDER CASES
+    for(; i < n; i++){
+        for(j = 0; j < n; j++){
+            A[j*n +(i)] = B[(i) * n + j];
+        }
+    }
+}
+
+
+void fill_random(float *M, int n) { //  generate random matrix values
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            M[i*n + j] = (float)rand() / RAND_MAX;
+}
+
+void zero_matrix(float *M, int n) { // instantiate an empty matrix for C
+    for (int i = 0; i < n*n; i++)
+        M[i] = 0.0f;
+}
+
+// Function for effective memory bandwidth:
+double calc_bandwidth(int n, double time_sec){
+    // read nxn floats and wr nxn floats
+    double bytes = 2.0 * n * n * sizeof(float);
+    return (bytes / time_sec) /1e9; // this is to get the result in Gigabyte/seconds
+}
+
+double benchmark(float *A, float *B, int n, int trials){
+    double t_start, t_end;
+    double total = 0.0;
+
+    for (int t = 0; t < trials; t++) {
+
+        zero_matrix(A, n);
+
+        get_walltime_(&t_start);
+        transpose_unroll_jam(A, B, n);
+        get_walltime_(&t_end);
+
+        total += (t_end - t_start);
+    }
+
+    return total / trials;
+}
+
+// printing the transposed matrix (at least a part of it)
+void print_mat(float *M, int n){
+    int limit = (n > 5) ? 5 : n; // is n > 5? if yes bring 5x5, else print n 
+    printf("=== MAT. SAMPLE ===\n");
+    for(int i = 0; i < limit; i++){
+        for(int j = 0; j < limit; j++){
+            printf("%6.2f ", M[i * n + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main(){
+    // AUTOMATED NOW
+    int sizes[] = {512, 1024, 2048, 4096};
+    int num_sizes = sizeof(sizes) / sizeof(sizes[0]);
+    int trials = 30;
+    printf("=== RUNNING ===\n");
+    printf("%-10s | %-15s | %-15s\n", "Size (n)", "Avg Time (s)", "Bandwidth (GB/s)");
+    printf("----------------------------------------------------------\n");
+    for (int i = 0; i < num_sizes; i++){
+        int n = sizes[i];
+        float *A = malloc(n * n * sizeof(float));
+        float *B = malloc(n * n * sizeof(float));
+        fill_random(B, n);
+
+        double avg_time = benchmark(A, B, n, trials);
+        double bandwidth = calc_bandwidth(n, avg_time);
+
+        printf("%-10d | %-15f | %-15.4f\n", n, avg_time, bandwidth);
+        free(A);
+        free(B);
+    }
+    return 0;
+    /*
+    display the original and transposed matrices: used earlier to check if transp is correct
+    printf("B, original:");
+    print_mat(B,n);
+
+    printf("A, transposed:");
+    transpose(A, B, n);
+    print_mat(A, n);
+
+    */
+    
+}
